9ef5dd5e4d0beb5c0a7f356b0c709f85942b13e0
ali-2465 <63744593+ali-2465@users.noreply.github.com>
2020-10-27 15:09:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const opts = {
     password: config.password
   },
   channels: [
-    "ali2465", "kunszg", "haxk", "alzexx", "fallenxy", "emredesu"
+    "ali2465", "kunszg", "haxk", "alzexx", "fallenxy", "emredesu", "okabar"
   ]
 };
 // Create a client with our options
