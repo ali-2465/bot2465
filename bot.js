8861a4a79147de434a135f601fc4f8db0bae13e8
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-10-27 11:08:15 +0100

    added restart command, fixed variable naming,  separated private keys into a config file

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const tmi = require('tmi.js');
+const config = require('./config.js');
 
 // Define configuration options
 const opts = {
   identity: {
     username: "bot2465",
-    password: "oauth:t0gcp8imlmysz5pm91ndeidw09350j"
+    password: config.password
   },
   channels: [
     "ali2465", "kunszg", "haxk", "alzexx", "fallenxy", "emredesu"
@@ -21,69 +22,88 @@ client.on('connected', onConnectedHandler);
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
+function onMessageHandler (channel, user, message, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
-  const commandName = msg.trim();
+  const commandName = message.trim();
 
   // If the command is known, let's execute it
   if (commandName === ')ping') {
-    client.say(target, `🅱ing :o`);
+    client.say(channel, `🅱ing :o`);
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
+    return;
+  } 
   if (commandName === ')info') {
-    client.say(target, `@${context['username']} I'm a simple chat bot that doesn't do anything useful, for now. Made by ali2465 :)`)
-      console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+    client.say(channel, `@${user['username']} I'm a simple chat bot that doesn't do anything useful, for now. Made by ali2465 :)`)
+    console.log(`* Executed ${commandName} command`);
+    return;
   }
   if (commandName === ')test') {
     const num = Math.floor(Math.random() * 999999999999) + 1; 
-    client.say(target, `Test! # ${num}`);
+    client.say(channel, `Test! # ${num}`);
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
+    return;
+  } 
   if (commandName === ')dice') {
     const num = Math.floor(Math.random() * 6) + 1; 
-    client.say(target, `🎲 You rolled a ${num}.`);
+    client.say(channel, `🎲 You rolled a ${num}.`);
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
+    return;
+  } 
   if (commandName === ')kill') {
-    client.say(target, `⣿⣿⣿⣿⣿⣿⣿⠟⣉⣉⣉⡉⠉⠙⠛⠿⣿⣿⣿⣿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⠋⣰⣿⣿⣿⣿⣿⣦⠄⠄⠄⠙⠋⢁⣠⣤⣄⠄⠄⠈⢻⣿⣿⣿ ⣿⣿⣿⣿⡟⢁⣾⣿⣿⣿⣿⣿⣿⣿⣷⣴⣤⣤⣶⣿⣿⣿⣿⣧⠄⠄⠂⢹⣿⣿ ⣿⣿⡿⠋⣴⣿⣿⣿⡿⠟⠛⠛⠿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⡆⠄⠄⠄⣿⣿ ⣿⠟⣠⣾⣿⣿⣿⠏⣰⣾⣿⣿⣷⡌⢻⣿⣿⣿⠋⣴⣶⣶⣶⣦⡁⠄⠄⠄⣿⣿ ⠏⠄⣛⣿⣿⣿⣿⡀⣿⣿⣦⣿⣿⡟⠘⣿⣿⣿⠘⣿⣿⣤⣿⣿⡇⠄⠄⠄⠘⢿ ⠄⢠⠁⡖⢬⡛⢯⠄⠈⠙⠛⠛⠉⠄⣰⣿⣿⣿⣄⠈⠙⠛⠛⠋⠄⠄⢀⡔⢲⠘ ⡆⠄⢢⡙⠦⣙⠶⠤⣄⣀⡀⠄⠴⠿⠿⢿⣿⣿⣿⣷⣤⣀⡀⠄⣀⠔⡁⠴⠖⢠ ⡇⠄⠄⢻⣶⣌⠳⢦⣄⡀⠉⠉⠉⠑⠒⠲⠶⠶⠶⠶⣶⣶⡶⠟⣡⠜⠄⠄⢰⣿ ⣿⠄⠄⠄⠻⣿⣿⣦⣍⡻⢷⣦⣄⡀⠄⠄⠄⠄⠄⠄⠄⢀⣠⡾⢃⠄⠄⠄⣸⣿ ⢋⣴⣆⠄⠄⠈⠻⢿⣿⣿⣶⣬⣉⡛⠷⣶⣦⣤⣤⣴⠶⠟⡋⠔⠁⠄⢀⣴⣿⣿ ⣿⣿⣿⣷⣦⣀⠄⠄⠉⠻⠿⢿⠿⢿⣷⣶⣤⠤⠤⠖⠒⠁⠄⠄⢀⣴⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣷⣦⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿ 枯れる`);
+    client.say(channel, `⣿⣿⣿⣿⣿⣿⣿⠟⣉⣉⣉⡉⠉⠙⠛⠿⣿⣿⣿⣿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⠋⣰⣿⣿⣿⣿⣿⣦⠄⠄⠄⠙⠋⢁⣠⣤⣄⠄⠄⠈⢻⣿⣿⣿ ⣿⣿⣿⣿⡟⢁⣾⣿⣿⣿⣿⣿⣿⣿⣷⣴⣤⣤⣶⣿⣿⣿⣿⣧⠄⠄⠂⢹⣿⣿ ⣿⣿⡿⠋⣴⣿⣿⣿⡿⠟⠛⠛⠿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⡆⠄⠄⠄⣿⣿ ⣿⠟⣠⣾⣿⣿⣿⠏⣰⣾⣿⣿⣷⡌⢻⣿⣿⣿⠋⣴⣶⣶⣶⣦⡁⠄⠄⠄⣿⣿ ⠏⠄⣛⣿⣿⣿⣿⡀⣿⣿⣦⣿⣿⡟⠘⣿⣿⣿⠘⣿⣿⣤⣿⣿⡇⠄⠄⠄⠘⢿ ⠄⢠⠁⡖⢬⡛⢯⠄⠈⠙⠛⠛⠉⠄⣰⣿⣿⣿⣄⠈⠙⠛⠛⠋⠄⠄⢀⡔⢲⠘ ⡆⠄⢢⡙⠦⣙⠶⠤⣄⣀⡀⠄⠴⠿⠿⢿⣿⣿⣿⣷⣤⣀⡀⠄⣀⠔⡁⠴⠖⢠ ⡇⠄⠄⢻⣶⣌⠳⢦⣄⡀⠉⠉⠉⠑⠒⠲⠶⠶⠶⠶⣶⣶⡶⠟⣡⠜⠄⠄⢰⣿ ⣿⠄⠄⠄⠻⣿⣿⣦⣍⡻⢷⣦⣄⡀⠄⠄⠄⠄⠄⠄⠄⢀⣠⡾⢃⠄⠄⠄⣸⣿ ⢋⣴⣆⠄⠄⠈⠻⢿⣿⣿⣶⣬⣉⡛⠷⣶⣦⣤⣤⣴⠶⠟⡋⠔⠁⠄⢀⣴⣿⣿ ⣿⣿⣿⣷⣦⣀⠄⠄⠉⠻⠿⢿⠿⢿⣷⣶⣤⠤⠤⠖⠒⠁⠄⠄⢀⣴⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣷⣦⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿ 枯れる`);
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+    return;
   } 
   if (commandName === ')commands') {
-    client.say(target, `)ping, )info, )test, )dice, )kill`);
+    client.say(channel, `)ping, )info, )test, )dice, )kill`);
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+    return;
   } 
   if (commandName === ')daily%') {
-    client.say(target, `$% dayave`);
+    client.say(channel, `$% dayave`);
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
+    return;
+  } 
   if (commandName === ')ec') {
-    client.say(target, `$gift cookie emredesu`);
+    client.say(channel, `$gift cookie emredesu`);
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }  
+    return;
+  } 
   if (commandName === '!xd') {
-    client.say(target, `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠛⠛⠛⠛⠛⠛⠿⠿⣿⣿⣿⣿⣿ ⣿⣿⣯⡉⠉⠉⠙⢿⣿⠟⠉⠉⠉⣩⡇⠄⠄⢀⣀⣀⡀⠄⠄⠈⠹⣿⣿⣿ ⣿⣿⣿⣷⣄⠄⠄⠈⠁⠄⠄⣠⣾⣿⡇⠄⠄⢸⣿⣿⣿⣷⡀⠄⠄⠘⣿⣿ ⣿⣿⣿⣿⣿⣶⠄⠄⠄⠠⣾⣿⣿⣿⡇⠄⠄⢸⣿⣿⣿⣿⡇⠄⠄⠄⣿⣿ ⣿⣿⣿⣿⠟⠁⠄⠄⠄⠄⠙⢿⣿⣿⡇⠄⠄⠸⠿⠿⠿⠟⠄⠄⠄⣰⣿⣿ ⣿⡿⠟⠁⠄⢀⣰⣶⣄⠄⠄⠈⠻⣿⡇⠄⠄⠄⠄⠄⠄⠄⢀⣠⣾⣿⣿⣿ ⣿⣷⣶⣶⣶⣿⣿⣿⣿⣷⣶⣶⣶⣿⣷⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿`);
+    client.say(channel, `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠛⠛⠛⠛⠛⠛⠿⠿⣿⣿⣿⣿⣿ ⣿⣿⣯⡉⠉⠉⠙⢿⣿⠟⠉⠉⠉⣩⡇⠄⠄⢀⣀⣀⡀⠄⠄⠈⠹⣿⣿⣿ ⣿⣿⣿⣷⣄⠄⠄⠈⠁⠄⠄⣠⣾⣿⡇⠄⠄⢸⣿⣿⣿⣷⡀⠄⠄⠘⣿⣿ ⣿⣿⣿⣿⣿⣶⠄⠄⠄⠠⣾⣿⣿⣿⡇⠄⠄⢸⣿⣿⣿⣿⡇⠄⠄⠄⣿⣿ ⣿⣿⣿⣿⠟⠁⠄⠄⠄⠄⠙⢿⣿⣿⡇⠄⠄⠸⠿⠿⠿⠟⠄⠄⠄⣰⣿⣿ ⣿⡿⠟⠁⠄⢀⣰⣶⣄⠄⠄⠈⠻⣿⡇⠄⠄⠄⠄⠄⠄⠄⢀⣠⣾⣿⣿⣿ ⣿⣷⣶⣶⣶⣿⣿⣿⣿⣷⣶⣶⣶⣿⣷⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿`);
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }     
+    return;
+  } 
+  if (commandName === ')restart' && (user['user-id'] === '194267009' || user['user-id'] === '178087241')) {
+    // check if current plaform is supported
+    if (process.platform === "win32") {
+      client.say(channel, 'This command cannot be ran outside of Linux, you should use it on server version of the bot :)');
+      return;
+    }
+
+    const shell = require('child_process');
+
+    // scrape github dry run response
+    const checkGithubUpdate = shell.execSync(`
+      sudo git remote update | sed '$d'; 
+      sudo git status -uno | awk '{if(NR>1)print}'
+      `).toString();
+
+    if (checkGithubUpdate.includes(`branch is up to date`)) {
+      client.say(channel, 'Bot is already up to date with repository :o');
+    } else {
+      // pull from main branch
+      shell.execSync('sudo git pull');
+      setTimeout(() => {
+        shell.execSync('sudo pm2 restart bot.js');
+        client.say(channel, 'restarting :)')
+      }, 1000);
+    }
+    return;
+  }
+  console.log(`* Unknown command ${commandName}`);
 }
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
