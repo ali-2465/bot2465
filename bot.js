30ff90ad41c3f173ce0122981b5aa9ed79f69266
ali-2465 <63744593+ali-2465@users.noreply.github.com>
2021-01-13 22:28:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,7 +120,7 @@ function onMessageHandler(channel, user, message, self) {
     return;
   } 
   if (commandName === ')xd123' && (user['user-id'] === '194267009')) {
-    client.say(channel, `/me ${user['username']}`);
+    client.say(channel, `/ban ${user['username']}`);
     return;
   }
   if (commandName === ')restart' && (user['user-id'] === '194267009' || user['user-id'] === '178087241')) {
