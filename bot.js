0052294d029c0d8efa4bf723ff8ec2dbdb1d3833
ali-2465 <63744593+ali-2465@users.noreply.github.com>
2021-10-19 17:24:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ function onMessageHandler(channel, user, message, self) {
       client.say(channel, 'No.');
       return;
     }
-    if (msg[1] === "bot2465" || msg[1] === "me" || msg[1] === "the bot") {
+    if (msg[1] === "bot2465" || msg[1] === "me" || msg[1] === "the") {
       client.say(channel, "You can't kill the bot! >(");
       return;
     }
